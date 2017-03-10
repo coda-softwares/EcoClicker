ce65512c6581ae4b9c978553d54e5976c308a423
jefferson <jefferson_bomfim@outlook.com>
2017-03-10 11:49:02 -0300

    Project Body

---
+++ android/src/com/codasoftwares/ecoc/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.codasoftwares.ecoc;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.codasoftwares.ecoc.EcoClicker;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new EcoClicker(), config);
+	}
+}
---
+++ desktop/src/com/codasoftwares/ecoc/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.codasoftwares.ecoc.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.codasoftwares.ecoc.EcoClicker;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new EcoClicker(), config);
+	}
+}
